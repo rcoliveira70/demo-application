a9496617a0a78c353dd50a4866a2041e90c7ca6d
Ricardo Ribeiro <rcribeiro@me.com>
2019-12-26 16:03:28 -0300

    Updates

---
+++ precache-manifest.7f70af654453814f0e5acef9ac23cb85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b032071282072a177610789b87d361a",
+    "url": "/demo-application/index.html"
+  },
+  {
+    "revision": "e3c6ff0a3914d2f9d0da",
+    "url": "/demo-application/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ac0859c600f170bb976b",
+    "url": "/demo-application/static/js/2.615ff657.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/demo-application/static/js/2.615ff657.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e3c6ff0a3914d2f9d0da",
+    "url": "/demo-application/static/js/main.421852c9.chunk.js"
+  },
+  {
+    "revision": "acd34d1a69d6d718ed65",
+    "url": "/demo-application/static/js/runtime-main.38c4b43e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo-application/static/media/logo.5d5d9eef.svg"
+  }
+]);
